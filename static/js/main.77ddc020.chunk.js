fb04e0990304f37d43d07050f8e23315e5512d5c
Parth Doshi <parthdoshi97@gmail.com>
2021-11-05 22:10:47 +0530

    remove github token

--- static/js/main.77ddc020.chunk.js
+++ static/js/main.77ddc020.chunk.js
@@ -141,9 +141,9 @@
                 }]
             },
             o = {
-                githubConvertedToken: "7d34b0c0d6002c4755ee23d8d92fc2434c3a89df",
-                githubUserName: "ParthKetanDoshi",
-                showGithubProfile: "false"
+                // githubConvertedToken: "",
+                // githubUserName: "ParthKetanDoshi",
+                // showGithubProfile: "false"
             },
             m = {
                 title: "MOOCs",
